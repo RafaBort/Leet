59acb35cd4321994a68197a53e1bfddbd1322c31
RafaBort <rafaelafonseca.bort2019ri@gmail.com>
2020-05-15 21:11:38 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Leet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite algo.....");
+            string frase1 = Console.ReadLine();
+            string frase2 = frase1.Replace("a","4").Replace("e","3").Replace("l","1").Replace("o","0").Replace("t","7").Replace("s","5");
+
+            Console.WriteLine(frase2);
+        }
+    }
+}
